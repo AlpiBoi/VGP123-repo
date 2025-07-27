aae69b0cffa4f9b6397c23b16299469c9b1c07da
AlpiBoi <adam.desro754@gmail.com>
2025-07-27 16:43:55 -0400

    Added collisions and player movement

---
+++ Assets/Scripts/player/playerController.cs
@@ -0,0 +1,24 @@
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hValue = Input.GetAxis("Horizontal");
+
+        rb.linearVelocityX = hValue * 5f;
+    }
+}
